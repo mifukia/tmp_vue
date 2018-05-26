96e6e08d12163f5a760c774995fc8b1d2770cdba
mifukia <mifukiahelp@gmail.com>
2018-05-26 17:34:19 +0900

    vue-router add

--- src/assets/js/modules/common.js
+++ src/assets/js/modules/common.js
@@ -1,17 +1,11 @@
 const $ = require("jquery");
 
 export default class Common {
-    constructor () {
+    constructor() {
         this.speed = 400;
     }
-    init(){
-        $('a[href^="#"]').on('click',e=>pageScroll(e.currentTarget));
+    init() {
+
     }
-    pageScroll(el){
-        let self,href,target,position;
-        href= $(el).attr("href");
-        target = $(href == "#" || href == "" ? 'html' : href);
-        position = target.offset().top;
-        $("html, body").animate({scrollTop:position}, this.speed);
-    }
-};
+
+}
--- src/assets/js/vue/App.js
+++ src/assets/js/vue/App.js
@@ -1,7 +1,10 @@
 import Vue from 'vue';
 import store from './store/store';
+import router from './router';
 import VueParent from './component/parent.vue';
+
 new Vue({
+    router,
     store,
     el: '#app',
     components: {
